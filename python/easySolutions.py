311a35909ac1012a5465199def082abf5282b52e
Ruben Mercado <rubennmercado@yahoo.com>
2022-05-10 12:55:41 -0400

    more python easy solutions

--- python/easySolutions.py
+++ python/easySolutions.py
@@ -19,4 +19,36 @@ def isAnagram(self, s, t):
         return False
 
 
+# 1. Two Sum
+# https://leetcode.com/problems/two-sum/
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+
+def twoSum(self, nums, target):
+        hashmap = {} #python dictionary
         
+        for i in range(len(nums)):
+            diff = target - nums[i] #finding target number pair
+            if diff in hashmap:  # check if target pair is in dict
+                return [i,hashmap[diff]] # return indices of the pairs ( i in this case and indicie saved as value in dict)
+            hashmap[nums[i]] = i # saving number as key in dict if a pair not found yet and its indicie as value
+
+
+# - Two Pointer
+
+# 125. Valid Palindrome
+# https://leetcode.com/problems/valid-palindrome/
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+def isPalindrome(self, s):
+        lst = []
+        Str = s.lower()  # lower casing string
+        for i in Str:
+            if i.isalnum(): # checking for only alphanumerics
+                lst.append(i)  # adding only alphanumerics to list
+        return lst == lst[::-1]  # checking if list is the same as the list backwards ([::-1] is reversing list)
+
+
